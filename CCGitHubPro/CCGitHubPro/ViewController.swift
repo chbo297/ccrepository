aeddb0bc2b4d90ce199c96291bd706c60271795e
ChengBo <chbo297@gmail.com>
2016-12-17 14:37:56 +0800

    create project

---
+++ CCGitHubPro/CCGitHubPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CCGitHubPro
+//
+//  Created by bo on 17/12/2016.
+//  Copyright © 2016 bo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
